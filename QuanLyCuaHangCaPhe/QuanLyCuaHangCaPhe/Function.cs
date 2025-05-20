4c57c5b47f52b452b35d823e264819b065e11831
DieuLinh-04 <25a4041864@hvnh.edu.vn>
2025-05-20 13:44:25 +0700

    tạo project

---
+++ QuanLyCuaHangCaPhe/QuanLyCuaHangCaPhe/Function.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangCaPhe
+{
+    internal class Function
+    {
+        public static SqlConnection Conn;  //Khai báo đối tượng kết nối
+        public static string connString;   //Khai báo biến chứa chuỗi kết nối
+
+        public static void Connect()
+        {
+            //Thiết lập giá trị cho chuỗi kết nối
+            connString = "Data Source=DESKTOP-PLEJFRL;Initial Catalog=QuanLyBanHangCaPhe;Integrated Security=True;";
+            Conn = new SqlConnection();                 //Cấp phát đối tượng
+            Conn.ConnectionString = connString;         //Kết nối
+            Conn.Open();                                //Mở kết nối
+        }
+
+        public static void Disconnect()
+        {
+            if (Conn.State == ConnectionState.Open)
+            {
+                Conn.Close();       //Đóng kết nối
+                Conn.Dispose();     //Giải phóng tài nguyên
+                Conn = null;
+            }
+        }
+        public static DataTable GetDataToTable(string sql)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, Function.Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            return table;
+        }
+
+        public static bool CheckKey(string sql)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, Function.Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            if (table.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static void RunSql(string sql)
+        {
+            SqlCommand cmd;
+            cmd = new SqlCommand();          // Khởi tạo đối tượng
+            cmd.Connection = Function.Conn;   // Gán kết nối
+            cmd.CommandText = sql;            // Gán câu lệnh SQL
+            try
+            {
+                cmd.ExecuteNonQuery();        // Thực hiện câu lệnh SQL
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Dispose();
+            cmd = null;
+        }
+
+        public static void RunSqlDel(string sql)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Function.Conn;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Dữ liệu đang được dùng, không thể xóa...", "Thông báo",
+                MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            cmd.Dispose();
+            cmd = null;
+        }
+
+        public static string ConvertDateTime(string d)
+        {
+            string[] parts = d.Split('/');
+            string dt = String.Format("{0}/{1}/{2}", parts[1], parts[0], parts[2]);
+            return dt;
+        }
+
+        public static string GetFieldValues(string sql)
+        {
+            string ma = "";
+            SqlCommand cmd = new SqlCommand(sql, Function.Conn);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                ma = reader.GetValue(0).ToString();
+            }
+            reader.Close();
+            return ma;
+        }
+        public static void FillCombo(string sql, ComboBox cbo, string ma, string ten)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            cbo.DataSource = table;
+
+            cbo.ValueMember = ma;    // Truong gia tri
+            cbo.DisplayMember = ten;    // Truong hien thi
+        }
+    }
+}
