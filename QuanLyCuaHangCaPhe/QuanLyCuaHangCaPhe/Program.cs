4d917249545269a9871458ae678aa2375b47046c
HoaiThu12 <25a4041506@hvnh.edu.vn>
2025-05-25 03:53:07 +0700

    quaylai

--- QuanLyCuaHangCaPhe/QuanLyCuaHangCaPhe/Program.cs
+++ QuanLyCuaHangCaPhe/QuanLyCuaHangCaPhe/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,3 +23,29 @@ namespace QuanLyCuaHangCaPhe
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangCaPhe
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Function.Connect();
+            Application.Run(new frmBaoCao());
+
+        }
+    }
+}
+>>>>>>> Stashed changes
